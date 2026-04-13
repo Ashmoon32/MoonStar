01fdf2a2f3ecc6ce23aaa5f35dae268677a9f0ae
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-04-13 16:30:55 +0630

    Initial commit

---
+++ resources/views/videos/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MoonStar Video Streaming Application</title>
+</head>
+<body>
+    <h1>Welcome to our <b><i>MoonStar</i></b>, <br>Video Streaming Application..</h1>
+    <p>Explore our <a href="/videos">Videos</a></p>
+</body>
+</html>
